0fa96a840f021a9528c07d8f80d765c02c9e0098
chumpon-asaneerat <chumponsenate@gmail.com>
2019-07-07 18:55:29 +0700

    SqlServer class implements query/execute method.

--- src/server/js/nlib/nlib-mssql.js
+++ src/server/js/nlib/nlib-mssql.js
@@ -43,6 +43,113 @@ const check_config = (name = 'default') => {
     return cfg.get('mssql.' + sName);
 }
 
+const get_type = (sStr, sidx) => {
+    return (sidx !== -1) ? sStr.substring(0, sidx).trim() : sStr.trim();
+}
+
+const get_params = (sStr, sidx, eidx) => {
+    return (sidx !== -1 && eidx !== -1) 
+        ? sStr.substring(sidx + 1, eidx).split(',').map(p => Number(p))
+        : null;
+}
+
+const extract = (str) => {
+    let sStr = str.trim().toLowerCase();
+    let sidx = sStr.indexOf('(');
+    let eidx = sStr.indexOf(')');
+    let result = {};
+    // get type name only (in string)
+    result.type = get_type(sStr, sidx);
+    // split all parameters
+    result.params = get_params(sStr, sidx, eidx)
+
+    return result;
+}
+
+/**
+ * Mapped Type Convert functions.
+ */
+const Convert = {
+    "nvarchar": mssql.NVarChar,
+    "nchar": mssql.NChar,
+    "varchar": mssql.VarChar,
+    "char": mssql.Char,
+    "ntext": mssql.NText,
+    "text": mssql.Text,
+
+    "bigint": mssql.BigInt,
+    "int": mssql.Int,
+    "smallint": mssql.SmallInt,
+    "tinyint": mssql.TinyInt,
+
+    "numeric": mssql.Numeric,
+    "decimal": mssql.Decimal,
+    "float": mssql.Float,
+    "real": mssql.Real,
+    "money": mssql.Money,
+    "smallmoney": mssql.SmallMoney,
+
+    "bit": mssql.Bit,
+    "varbinary": mssql.VarBinary,
+    "binary": mssql.Binary,
+    "image": mssql.Image,
+
+    "datetime": mssql.DateTime,
+    "date": mssql.Date,
+    "time": mssql.Time,
+    "smalldatetime": mssql.SmallDateTime,
+    "datetime2": mssql.DateTime2,
+    "datetimeoffset": mssql.DateTimeOffset,
+
+    "guid": mssql.UniqueIdentifier,
+    "UniqueIdentifier": mssql.UniqueIdentifier
+}
+
+const get_1_params = (tObj) => { 
+    return (tObj.params && tObj.params.length >= 1) ? tObj.params[0] : null;
+}
+
+const get_2_params = (tObj) => { 
+    return (tObj.params && tObj.params.length >= 2) ? tObj.params[1] : null;
+}
+
+const parse = (str) => {
+    let tObj = extract(str);
+    let sType = tObj.type;
+    let p1 = get_1_params(tObj);
+    let p2 = get_2_params(tObj);
+    return (p1) ? (p2) ? Convert[sType](p1, p2) : Convert[sType](p1) : Convert[sType]();
+}
+
+const checkInputs = (inputs) => {
+    let ret = true;
+    if (!inputs || inputs.length <= 0) ret = false;
+    return ret;
+}
+const checkOutputs = (outputs) => {
+    let ret = true;
+    if (!outputs || outputs.length <= 0) ret = false;
+    return ret;
+}
+
+const prepareInputs = (rq, inputs) => {
+    if (rq) {
+        if (checkInputs(inputs)) {
+            inputs.forEach(p => {
+                req.input(p.name, parse(p.type), p.value);
+            });
+        }
+    }
+}
+const prepareOutputs = (rq, outputs) => {
+    if (rq) {
+        if (checkOutputs(inputs)) {
+            outputs.forEach(p => {
+                req.input(p.name, parse(p.type), p.value);
+            });
+        }
+    }
+}
 
 //#endregion
 
@@ -92,17 +199,35 @@ const SqlServer = class {
     /**
      * Run Query.
      */
-    async query() {
-        let ret;
-        if (!this.connected) ret = false;
+    async query(opts, pObj) {
+        let ret = {};
+        if (this.connected) {
+            let ps = new mssql.PreparedStatement(this.connection);
+
+            prepareInputs(ps, opts.inputs);
+            prepareOutputs(ps, opts.outputs);
+
+            await ps.prepare(opts.text);
+            ret = await ps.execute(opts.value);
+            await ps.unprepare();
+        }
+
         return ret;
     }
     /**
      * Execute Stored Procedure.
      */
-    async execute() {
-        let ret;
-        if (!this.connected) ret = false;
+    async execute(opts, pObj) {
+        let ret = {};
+        if (!this.connected) {
+            let req = new mssql.Request(this.connection);
+
+            prepareInputs(req, opts.inputs);
+            prepareOutputs(req, opts.outputs);
+
+            ret = await req.execute(opts.name);
+        }
+
         return ret;
     }
     /**
