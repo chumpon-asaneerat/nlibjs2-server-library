54c03ab7fd374a8e7ecef3749a242f1bde39f2b9
chumpon-asaneerat <chumponsenate@gmail.com>
2019-07-08 12:27:24 +0700

    Remove unused code and spaces.

--- server.js
+++ server.js
@@ -198,29 +198,3 @@ if (svr) {
 }
 
 //#endregion
-
-//#region Original Express seting code
-/*
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const bodyparser = require("body-parser");
-const cookieparser = require("cookie-parser");
-
-const app = express();
-
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(cookieparser("YOUR_SECURE_KEY@123"));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.status(200).send(`It's work!!!`);
-});
-
-const server = app.listen(nlib.config.webserver.port, () => {
-    console.log(`${nlib.config.app.name} listen on port: ${nlib.config.webserver.port}`);
-});
-*/
-//#endregion
