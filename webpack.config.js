92b61eb666ed7126f1a2335ae0960df528661acd
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-01 16:58:50 +0700

    Update webpack.config.js to supports scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
@@ -32,6 +33,22 @@ module.exports = {
                 options: { presets: ['@babel/preset-env'] }
             }
         }, {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    // fallback to style-loader in development
+                    loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                },
+                {
+                    loader: 'css-loader',
+                    options: { modules: false }
+                },
+                {
+                    loader: "sass-loader"
+                }
+            ]
+        }, {
             test: /\.css$/,
             exclude: /node_modules/,
             use: [
@@ -46,5 +63,13 @@ module.exports = {
                 }
             ]
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 }
