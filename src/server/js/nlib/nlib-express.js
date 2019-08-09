0ff186ecb7cbfc89310aba50e40dc60959afb920
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-09 16:50:53 +0700

    Copy NJwtService class from nlib.js (comment out to reimplements).

--- src/server/js/nlib/nlib-express.js
+++ src/server/js/nlib/nlib-express.js
@@ -575,3 +575,22 @@ module.exports.RequestHandler = exports.RequestHandler = express.RequestHandler;
 module.exports.uploadfiles = exports.uploadfiles = uploadfiles;
 
 //#endregion
+
+/*
+class NJwtService {
+    validateDevice(req, res, next) {
+        let name = `x-device`;
+        let data = ncookie.parse(req, name);
+        if (!data) {
+            let token = uuidv4();
+            ncookie.store(res, name, token);
+        }
+        next();
+    };
+    signin(req, res, next) {
+    };
+    signout(req, res, next) {
+    };
+
+};
+*/
