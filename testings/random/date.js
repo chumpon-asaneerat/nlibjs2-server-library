86ddd9cf5fdd83f1d3c9b163f062444d939523bd
chumpon-asaneerat <chumponsenate@gmail.com>
2020-01-11 10:50:16 +0700

    Update date.js random sample.

--- testings/random/date.js
+++ testings/random/date.js
@@ -1,2 +1,9 @@
 const nlib = require('../.././src/server/js/nlib/nlib');
 const rand = nlib.NRandom;
+
+let beginDate = new Date('2020-01-01')
+//console.log(beginDate)
+let endDate = new Date('2020-12-31')
+//console.log(endDate)
+let ret = rand.date(beginDate, endDate, 3)
+console.log(JSON.stringify(ret))
