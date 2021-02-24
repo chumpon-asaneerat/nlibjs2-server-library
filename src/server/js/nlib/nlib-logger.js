867392f25f4d2667b8de20e8131cbe060491857c
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-24 10:32:32 +0700

    Update 2021-02-24 #5 - Implements nlib-logger.js #2

--- src/server/js/nlib/nlib-logger.js
+++ src/server/js/nlib/nlib-logger.js
@@ -3,3 +3,33 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf, colorize, prettyPrint } = format;
 const colorizer = winston.format.colorize();
 const DailyRotateFile = require('winston-daily-rotate-file');
+
+/*
+const log_file_opts =  {
+    filename: 'application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+}
+
+const logger = createLogger({
+    // general format.
+    format: combine(
+        timestamp(),
+        logFormat),
+    transports: [
+        new transports.Console({
+            // custom format for console.
+            format: combine(
+                colorize({all: true}),
+                timestamp(),
+                prettyPrint(),
+                logFormat)
+        }),
+        //new transports.File({ filename: 'combined.log' }),
+        new DailyRotateFile(log_file_opts)
+    ]        
+})
+
+*/
