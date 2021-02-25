394e8ed3d83cfaab06d606f6d958ed0daadc7b37
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-25 16:10:23 +0700

    Update 2021-02-25 #3 - Implements nlib-logger #1.

--- src/server/js/nlib/nlib-logger.js
+++ src/server/js/nlib/nlib-logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf, colorize, prettyPrint } = format;
-const colorizer = winston.format.colorize();
+//const colorizer = winston.format.colorize();
 const DailyRotateFile = require('winston-daily-rotate-file');
 
 /*
